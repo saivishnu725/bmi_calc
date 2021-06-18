06a29496eafbc81b272a4b1c1641e6e5b30b8ac1
Sai Vishnu <saivishnu725@gmail.com>
2021-06-18 21:19:33 +0530

    Added a nice looking container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,10 +12,19 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("BMI Calculator"),
       ),
-      body: Center(
-        child: Text("Body Text"),
+      body: SafeArea(
+        child: Container(
+          height: 200.0,
+          width: 170.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Color(0xFF1D1E33),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
